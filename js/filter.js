ee61dd6ada2ea9c85f74b1403f1ed2e54dddd551
Jianming Qu <jianming.qu@rea-group.com>
2016-08-16 11:07:11 +0800

    lowercase compare

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,14 @@
+function contains(arr, str) {
+    var lowerCaseStr = str.toLowerCase();
+    return arr.find(_ => _.toLowerCase() === lowerCaseStr);
+}
+
 (function ($) {
     chrome.storage.sync.get("whitelist", function(data) {
         chrome.storage.sync.clear();
         var table = $("table#assignment");
         table.find("tbody tr")
-            .filter((i, l) => !data.whitelist.includes($(l).find(".name a").text()))
+            .filter((i, l) => !contains(data.whitelist, $(l).find(".name a").text()))
             .each((i, l) => $(l).hide());
     });
 })(jQuery);
