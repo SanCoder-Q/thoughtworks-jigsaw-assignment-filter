0feef2486bbc4222c797ab1bdd795d5533f7dc70
Jianming Qu <jianming.qu@rea-group.com>
2016-08-17 19:07:23 +0800

    update tracking

--- js/track.js
+++ js/track.js
@@ -1,8 +1,9 @@
-var username = $(".user-menu .username").text();
 var tabUrl = location.href;
 if(tabUrl.match(/jigsaw\.thoughtworks.+assignment/) && window._tu != tabUrl) {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.text = `var _gaq=_gaq||[];_gaq.push(['_setAccount','UA-58271438-2']);_gaq.push(['_trackPageview']);_gaq.push(['_setCustomVar',1,'userId','${username}',3]);var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;ga.src='https://ssl.google-analytics.com/ga.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);`;
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var elem = $(".user-menu .username");
+    var username = elem && elem.text && elem.text().trim();
+    var info = { name: username, url: tabUrl };
+    chrome.storage.local.set({track: info});
+    console.log(username);
     window._tu = tabUrl;
 }
