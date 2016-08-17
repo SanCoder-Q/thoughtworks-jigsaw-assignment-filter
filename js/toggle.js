4a79bf79a7bdc0beada172fb42fa4031d1abf0fe
Jianming Qu <jianming.qu@rea-group.com>
2016-08-17 17:00:50 +0800

    only goto the config page when first installed

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,10 @@
 chrome.runtime.onInstalled.addListener(function (object) {
-    chrome.tabs.create({url: "config.html"});
+    chrome.storage.sync.get(["installed"], function(data) {
+        if(!data.installed) {
+            chrome.storage.sync.set({installed: true});
+            chrome.tabs.create({url: "config.html"});
+        }
+    });
 });
 
 var executeWithDependencies = function(dependencies){
